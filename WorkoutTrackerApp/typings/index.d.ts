bba4ba71a9d8934eafb12e6ba8df2508a0dd9b57
Dan Van Horn <vanhornd@oregonstate.edu>
2016-11-21 18:52:59 -0800

    added typings

---
+++ WorkoutTrackerApp/typings/index.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="globals/express-handlebars/index.d.ts" />
+/// <reference path="globals/express/index.d.ts" />
+/// <reference path="globals/handlebars/index.d.ts" />
+/// <reference path="globals/node/index.d.ts" />
