39dcc0f1edaf042cfdffab0c1622788b700f9b74
Dan Van Horn <vanhornd@oregonstate.edu>
2016-11-27 21:43:58 -0800

    integrated grunt to automate view precompilation

---
+++ WorkoutTrackerApp/Gruntfile.js
@@ -0,0 +1,25 @@
+module.exports = function(grunt) {
+
+    
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON("package.json"),
+
+        handlebars: {
+            compile:{
+                options: {
+                    namespace: "content.Scripts"
+                },
+                files: {
+                    "content/Scripts/sport-row-template.js": "views/partials/sport-table-row.handlebars",
+                    "content/Scripts/weight-row-template.js": "views/partials/weight-table-row.handlebars"
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-handlebars');
+
+    grunt.registerTask('default', ["handlebars"])
+}
+
