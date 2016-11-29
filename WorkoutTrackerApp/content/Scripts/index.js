f6fdef01f2bf22900076be42adabd8ad614534e4
Dan Van Horn <vanhornd@oregonstate.edu>
2016-11-28 23:02:46 -0800

    implemented a date picker for modals

--- WorkoutTrackerApp/content/Scripts/index.js
+++ WorkoutTrackerApp/content/Scripts/index.js
@@ -1,17 +1,32 @@
+
+$('#weight-datetime').datetimepicker({ 
+    format: 'L'
+});
+
+$('#sport-datetime').datetimepicker({ 
+    format: 'L'
+});
+
+
 /* Listeners for displaying the "add" modals */
 
 $('#add-weight').on('click', function(event){
     $('#add-workout-modal').modal('show');
+    //add function to validate data on save here?
 });
 
 $('#add-run').on('click', function(event){
     $('#add-sport-modal').modal('show');
+    //add function to validate data on save here?
 });
 
 $('#add-cycle').on('click', function(event){
     $('#add-sport-modal').modal('show');
+    //add function to validate data on save here?
 });
 
 $('#add-swim').on('click', function(event){
     $('#add-sport-modal').modal('show');
+    //add function to validate data on save here?
 });
+
--- WorkoutTrackerApp/server.js
+++ WorkoutTrackerApp/server.js
@@ -3,6 +3,7 @@ var fs = require('fs');
 var path = require('path');
 var express = require('express');
 var exphbs = require('express-handlebars');
+var bodyParser = require('body-parser');
 
 var home = require('./controllers/home');
 var profile = require('./controllers/profile');
@@ -16,8 +17,10 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'handlebars');
 
+app.use(bodyParser.json());
 //serve files from ./content/
 app.use(express.static(path.join(__dirname, 'content')));
+app.use(express.static(path.join(__dirname, 'bower_components')));
 
 // Use routing controllers for home and profile
 app.use('/', home);
