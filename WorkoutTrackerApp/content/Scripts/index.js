7f4484bf0b4f2beeb41339f1d4fafda20f9e0a50
Kevin Turkington <turkingtonkevin@gmail.com>
2016-11-29 23:25:16 -0800

    dynamic row function, TODO: search rows by date

--- WorkoutTrackerApp/content/Scripts/index.js
+++ WorkoutTrackerApp/content/Scripts/index.js
@@ -3,6 +3,13 @@ var WorkOutModal = $('#add-workout-modal')
 var SportModal = $('#add-sport-modal');
 var workoutSaveButton = $('#save-workout');
 
+//workout modal inputs
+var dateInput = $('#weight-datetime');
+var exerciseInput = $('#w-input-exercise');
+var weightInput = $('#w-input-weight');
+var setsInput = $('#w-input-sets');
+var repsInput = $('#w-input-reps');
+
 
 $('#weight-datetime').datetimepicker({ 
     format: 'L'
@@ -24,45 +31,35 @@ $('#add-weight').on('click', function(event){
 
     workoutSaveButton.on('click',function(event){
         if(validateWorkOutModal() == true){
-            
+            //build row
+            buildWorkoutRow(dateInput.val(),
+                            exerciseInput.val(),
+                            weightInput.val(),
+                            setsInput.val(),
+                            repsInput.val());
+
+            //find correct row to insert
         }
     });
-
-    
-
-
-    //add function to validate data on save here?
 });
 
 function validateWorkOutModal(){
     var valid = true;
-    var dateInput = $('#weight-datetime');
-    var exerciseInput = $('#w-input-exercise');
-    var weightInput = $('#w-input-weight');
-    var setsInput = $('#w-input-sets');
-    var repsInput = $('#w-input-reps');
-
     if (dateInput.val() == null || dateInput.val() == '') {
         valid = false;
     }
-
     if (exerciseInput.val() == null || exerciseInput.val() == '') {
         valid = false;
     }
-
     if (weightInput.val() == null || weightInput.val() == '') {
         valid = false;
     }
-
     if (setsInput.val() == null || setsInput.val() == '') {
         valid = false;
     }
-
     if (repsInput.val() == null || repsInput.val() == '') {
         valid = false;
     }
-
-
 
     if(valid == false){
         alert('All textboxes must be filled.');
@@ -71,6 +68,24 @@ function validateWorkOutModal(){
     else{
         return valid;
     }
+}
+
+function buildWorkoutRow(date,exercise,weight,sets,reps){
+    var row = $(document.createElement('TR'));
+    row.append($(document.createElement("td")).text(date));
+    row.append($(document.createElement("td")).text(exercise));
+    row.append($(document.createElement("td")).text(weight));
+    row.append($(document.createElement("td")).text(sets));
+    row.append($(document.createElement("td")).text(reps));
+
+    //console.log(row);
+    return row;
+}
+
+function searchRowIndexBydate(table,date){
+     var tbody = $(table + ' >  tbody');
+
+
 }
 
 $('#add-run').on('click', function(event){
@@ -87,4 +102,3 @@ $('#add-swim').on('click', function(event){
     SportModal.modal('show');
     //add function to validate data on save here?
 });
-
