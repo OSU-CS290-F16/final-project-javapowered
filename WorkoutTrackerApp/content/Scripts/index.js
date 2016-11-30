cdef20b6e645faeaae7f4f2875051f5da5e58f40
Kevin Turkington <turkingtonkevin@gmail.com>
2016-11-29 21:50:07 -0800

    validation for workout modal

--- WorkoutTrackerApp/content/Scripts/index.js
+++ WorkoutTrackerApp/content/Scripts/index.js
@@ -1,3 +1,8 @@
+//Globals
+var WorkOutModal = $('#add-workout-modal')
+var SportModal = $('#add-sport-modal');
+var workoutSaveButton = $('#save-workout');
+
 
 $('#weight-datetime').datetimepicker({ 
     format: 'L'
@@ -11,22 +16,75 @@ $('#sport-datetime').datetimepicker({
 /* Listeners for displaying the "add" modals */
 
 $('#add-weight').on('click', function(event){
-    $('#add-workout-modal').modal('show');
+    WorkOutModal.modal('show');
+    
+    //removes previous event listener
+    workoutSaveButton.unbind();
+    //workoutSaveButton.off();
+
+    workoutSaveButton.on('click',function(event){
+        if(validateWorkOutModal() == true){
+            
+        }
+    });
+
+    
+
+
     //add function to validate data on save here?
 });
 
+function validateWorkOutModal(){
+    var valid = true;
+    var dateInput = $('#weight-datetime');
+    var exerciseInput = $('#w-input-exercise');
+    var weightInput = $('#w-input-weight');
+    var setsInput = $('#w-input-sets');
+    var repsInput = $('#w-input-reps');
+
+    if (dateInput.val() == null || dateInput.val() == '') {
+        valid = false;
+    }
+
+    if (exerciseInput.val() == null || exerciseInput.val() == '') {
+        valid = false;
+    }
+
+    if (weightInput.val() == null || weightInput.val() == '') {
+        valid = false;
+    }
+
+    if (setsInput.val() == null || setsInput.val() == '') {
+        valid = false;
+    }
+
+    if (repsInput.val() == null || repsInput.val() == '') {
+        valid = false;
+    }
+
+
+
+    if(valid == false){
+        alert('All textboxes must be filled.');
+        return valid;
+    }
+    else{
+        return valid;
+    }
+}
+
 $('#add-run').on('click', function(event){
-    $('#add-sport-modal').modal('show');
+    SportModal.modal('show');
     //add function to validate data on save here?
 });
 
 $('#add-cycle').on('click', function(event){
-    $('#add-sport-modal').modal('show');
+    SportModal.modal('show');
     //add function to validate data on save here?
 });
 
 $('#add-swim').on('click', function(event){
-    $('#add-sport-modal').modal('show');
+    SportModal.modal('show');
     //add function to validate data on save here?
 });
 
